aef55c40b944f4094225d49c2ef5a34774604ff3
Tin <thanhtinpk092007@gmail.com>
2018-10-24 18:10:08 +0700

    Allow all host

--- soundcloud_client/settings.py
+++ soundcloud_client/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e#z90g(s(z@t-y_@h-^o&plg4j!)^5wr$%)*o!%3e^7k*btoow'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
